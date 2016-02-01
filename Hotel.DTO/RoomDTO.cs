6ae34e9f6bfa72bc4055d939cb8901555e99db07
Wawrzak Adrian <adrian.wawrzak@jcommerce.pl>
2016-02-01 16:18:08 +0100

    repository, organization of files

---
+++ Hotel.DTO/RoomDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.DTO
+{
+    public class RoomDTO
+    {
+        public string Type { get; set; }
+        public string Quality { get; set; }
+        public int Capacity { get; set; }
+        public IEnumerable<string> Features { get; set; } 
+    }
+}
---
+++ Hotel.Logic/Repositories/RoomRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.DTO;
+using Hotel.Logic.Common;
+using Hotel.Logic.Utils;
+using NHibernate.Linq;
+
+namespace Hotel.Logic
+{
+    public class RoomRepository : Repository<Room>
+    {
+        public IEnumerable<RoomDTO> GetAll()
+        {
+            using (var session = SessionFactory.OpenSession())
+            {
+                return session.Query<Room>().ToList().Select(
+                    x => new RoomDTO
+                    {
+                        Capacity = x.Capacity,
+                        Features = x.Features.Select(y => y.Name),
+                        Quality = x.Quality.Quality.ToString(),
+                        Type = x.Type.Name.ToString(),
+                    }
+                    );
+            }
+        }
+    }
+}
